9c65d91a481ecc6342979e27987b8c7b501daa6d
ShuiZhuyuAlex <shuizhuyualex@gmail.com>
2019-03-27 17:29:50 -0400

    first

---
+++ flask/app/views.py
@@ -0,0 +1,10 @@
+from app import app
+import json
+from flask import render_template
+
+@app.route('/')
+@app.route('/radicaltree/')
+def RadicalTree():
+    with open("/Users/shuizhuyu/Desktop/GQP/backend/flask/app/static/alldata.json") as load_file:
+        data = json.load(load_file)
+    return render_template("Tree_v3.html",variable=data)
