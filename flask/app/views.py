bec78e9d271bd7195df7b7381f783f816d33def9
ShuiZhuyuAlex <shuizhuyualex@gmail.com>
2019-04-16 21:36:14 -0400

    merge

--- flask/app/views.py
+++ flask/app/views.py
@@ -3,20 +3,14 @@ import json
 from flask import render_template
 
 @app.route('/')
-<<<<<<< HEAD
-@app.route('/radicaltree1/')
-def RadicalTree_v1():
+@app.route('/radialtree1/')
+def RadialTree_v1():
     with open("/Users/shuizhuyu/Desktop/GQP/radicaltree/flask/app/static/alldata.json") as load_file:
         data = json.load(load_file)
     return render_template("Tree_v3.html",variable=data)
 
-@app.route('/radicaltree2/')
-def RadicalTree_v2():
+@app.route('/radialtree2/')
+def RadialTree_v2():
     with open("/Users/shuizhuyu/Desktop/GQP/radicaltree/flask/app/static/alldata_v2.json") as load_file:
-=======
-@app.route('/radicaltree/')
-def RadicalTree():
-    with open("/home/zsun/radicaltree/flask/app/static/alldata.json") as load_file:
->>>>>>> e18326b874a9aa3ac1641b56310f8e1fed0b7541
         data = json.load(load_file)
     return render_template("Tree_v3.html",variable=data)
