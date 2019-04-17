daa784157db945a657551309bbc51d6312ed5adb
ShuiZhuyuAlex <shuizhuyualex@gmail.com>
2019-04-16 21:32:20 -0400

    two website

--- flask/app/views.py
+++ flask/app/views.py
@@ -3,8 +3,14 @@ import json
 from flask import render_template
 
 @app.route('/')
-@app.route('/radicaltree/')
-def RadicalTree():
-    with open("/Users/shuizhuyu/Desktop/GQP/backend/flask/app/static/alldata.json") as load_file:
+@app.route('/radicaltree1/')
+def RadicalTree_v1():
+    with open("/Users/shuizhuyu/Desktop/GQP/radicaltree/flask/app/static/alldata.json") as load_file:
+        data = json.load(load_file)
+    return render_template("Tree_v3.html",variable=data)
+
+@app.route('/radicaltree2/')
+def RadicalTree_v2():
+    with open("/Users/shuizhuyu/Desktop/GQP/radicaltree/flask/app/static/alldata_v2.json") as load_file:
         data = json.load(load_file)
     return render_template("Tree_v3.html",variable=data)
